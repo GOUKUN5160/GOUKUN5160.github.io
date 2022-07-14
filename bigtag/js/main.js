a4a05f71dfa44285c30a97ae60ff30d942b70752
GOUKUN5160 <gouichirou.yamada@gmail.com>
2022-07-14 19:35:11 +0900

    commit4

--- bigtag/js/main.js
+++ bigtag/js/main.js
@@ -2,15 +2,12 @@ function registerInstallAppEvent(elem) {
 	window.addEventListener("beforeinstallprompt", function (event) {
 		event.preventDefault();
 		elem.promptEvent = event;
-		console.log("event!");
-		console.log(elem.promptEvent);
 		document.getElementById("install").hidden = false;
 		return false;
 	});
 
 	elem.addEventListener("click", 	function () {
 		if (elem.promptEvent) {
-			console.log("exec");
 			elem.promptEvent.prompt();
 			elem.promptEvent.userChoice.then(function(choice) {
 				elem.promptEvent = null;
